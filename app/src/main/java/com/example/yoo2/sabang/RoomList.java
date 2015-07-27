869d56c91ea23adf01b05171c2b352976fe942f3
yoo2 <yoo2yoo2yoo2@gmail.com>
2015-07-27 17:32:24 +0900

    master rere by ksh

---
+++ app/src/main/java/com/example/yoo2/sabang/RoomList.java
@@ -0,0 +1,16 @@
+package com.example.yoo2.sabang;
+
+/**
+ * Created by yoo2 on 15. 7. 23..
+ */
+public class RoomList {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public RoomList(String name){
+        this.name=name;
+    }
+}
